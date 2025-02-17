5863f055adbddd4cf0f5afeda388633aa4ce40b4
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-02-17 17:42:50 -0300

    DialogueManager Script

---
+++ Visual Novel/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private bool dialoguePlaying = false;
+
+    private void OnEnable()
+    {
+        GameEventsManager.Instance.dialogueEvents.onEnterDialogue += DialogueEvents_OnEnterDialogue;
+    }
+
+    private void OnDisable()
+    {
+        GameEventsManager.Instance.dialogueEvents.onEnterDialogue -= DialogueEvents_OnEnterDialogue;
+    }
+
+    private void DialogueEvents_OnEnterDialogue(string knotName)
+    {
+        dialoguePlaying = true;
+    }
+}
