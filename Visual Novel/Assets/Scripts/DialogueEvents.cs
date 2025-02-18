4fae0bf0a2d61c70277894fb179aec957f2d1e01
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-02-18 17:29:50 -0300

    onDialogueStarted, onDisplayDialogue and  onDialogueFinished Events
    DialogueStarted Method
    DisplayDialogue Method
    DialogueFinished Method

--- Visual Novel/Assets/Scripts/DialogueEvents.cs
+++ Visual Novel/Assets/Scripts/DialogueEvents.cs
@@ -3,9 +3,24 @@ using System;
 public class DialogueEvents 
 {
    public event Action<string> onEnterDialogue;
+   public event Action onDialogueStarted;
+   public event Action<string> onDisplayDialogue;
+   public event Action onDialogueFinished;
 
    public void EnterDialogue(string knotName)
    {
       onEnterDialogue?.Invoke(knotName);
+   }
+   public void DialogueStarted()
+   {
+      onDialogueStarted?.Invoke();
+   }
+   public void DisplayDialogue(string dialogueLine)
+   {
+      onDisplayDialogue?.Invoke(dialogueLine);
+   }
+   public void DialogueFinished()
+   {
+      onDialogueFinished?.Invoke();
    }
 }
