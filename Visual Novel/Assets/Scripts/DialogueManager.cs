c4ee10fd6170697de866d3b027373e4c66fe0391
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-02-18 15:53:13 -0300

    Ink Story
    story = new Story(inkJson.text);
    story.ChoosePathString(knotName);

--- Visual Novel/Assets/Scripts/DialogueManager.cs
+++ Visual Novel/Assets/Scripts/DialogueManager.cs
@@ -1,9 +1,19 @@
 using System;
 using UnityEngine;
-
+using Ink.Runtime;
 public class DialogueManager : MonoBehaviour
 {
+    [Header("Ink Story")] 
+    
+    [SerializeField] private TextAsset inkJson;
+
+    private Story story;
     private bool dialoguePlaying = false;
+
+    private void Awake()
+    {
+        story = new Story(inkJson.text);
+    }
 
     private void OnEnable()
     {
@@ -18,9 +28,11 @@ public class DialogueManager : MonoBehaviour
     private void DialogueEvents_OnEnterDialogue(string knotName)
     {
         if(dialoguePlaying) return;
+        if(knotName.Equals("")) return;
+        
+        Debug.Log("Ingresando al dialogo [Nombre del Knot] " + knotName);
         
         dialoguePlaying = true;
-
-        Debug.Log("Ingresando al dialogo [Nombre del Knot] " + knotName);
+        story.ChoosePathString(knotName);
     }
 }
