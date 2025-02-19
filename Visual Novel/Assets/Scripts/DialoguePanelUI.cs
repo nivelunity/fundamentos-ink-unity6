460b9a6bc29608243ff4ec7f53559d9ee68afc04
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-02-19 17:30:52 -0300

    Iterate dialogueChoices
    DialogueChoiceButton[] choiceButtons
    Choice dialogueChoice = dialogueChoices[inkChoiceIndex];

--- Visual Novel/Assets/Scripts/DialoguePanelUI.cs
+++ Visual Novel/Assets/Scripts/DialoguePanelUI.cs
@@ -7,6 +7,7 @@ public class DialoguePanelUI : MonoBehaviour
 {
     [SerializeField] private GameObject contentParent;
     [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private DialogueChoiceButton[] choiceButtons;
 
     private void Awake()
     {
@@ -41,6 +42,39 @@ public class DialoguePanelUI : MonoBehaviour
     private void DisplayDialogue(string dialogueLine, List<Choice> dialogueChoices)
     {
         dialogueText.text = dialogueLine;
+
+        if (dialogueChoices.Count > choiceButtons.Length)
+        {
+            Debug.LogError("More dialogue choices ("+dialogueChoices.Count+") came through than are supported("+ choiceButtons.Length+").");
+        }
+        
+        // Hidden Choice Buttons
+        foreach (DialogueChoiceButton choiceButton in choiceButtons)
+        {
+            choiceButton.gameObject.SetActive(false);
+        }
+        
+        // Set Ink choice Information
+        int choiceButtonIndex = dialogueChoices.Count - 1;
+        
+        for (int inkChoiceIndex = 0; inkChoiceIndex < dialogueChoices.Count; inkChoiceIndex++)
+        {
+            Choice dialogueChoice = dialogueChoices[inkChoiceIndex];
+            DialogueChoiceButton choiceButton = choiceButtons[choiceButtonIndex];
+            
+            choiceButton.gameObject.SetActive(true);
+            choiceButton.SetChoiceText(dialogueChoice.text);
+            choiceButton.SetChoiceIndex(inkChoiceIndex);
+
+            if (inkChoiceIndex == 0)
+            {
+                choiceButton.SelectButton();
+                GameEventsManager.Instance.dialogueEvents.UpdateChoiceIndex(0);
+            }
+            
+            choiceButtonIndex--;
+        }
+
     }
 
     private void ResetPanel()
