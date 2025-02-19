66efeb0875775bd2a58f3772b771fcd851608954
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-02-19 16:32:44 -0300

    UpdateChoiceIndex Event

--- Visual Novel/Assets/Scripts/DialogueEvents.cs
+++ Visual Novel/Assets/Scripts/DialogueEvents.cs
@@ -5,7 +5,8 @@ public class DialogueEvents
    public event Action<string> onEnterDialogue;
    public event Action onDialogueStarted;
    public event Action<string> onDisplayDialogue;
-   public event Action onDialogueFinished;
+   public event Action onDialogueFinished; 
+   public event Action<int> onUpdateChoiceIndex;
 
    public void EnterDialogue(string knotName)
    {
@@ -22,5 +23,9 @@ public class DialogueEvents
    public void DialogueFinished()
    {
       onDialogueFinished?.Invoke();
+   }
+   public void UpdateChoiceIndex(int choiceIndex)
+   {
+      onUpdateChoiceIndex?.Invoke(choiceIndex);
    }
 }
