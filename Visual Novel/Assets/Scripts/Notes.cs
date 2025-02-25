a232d5c0b42745dd1a2ddfefd57c3d4ce374cad2
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-02-25 17:00:01 -0300

    Notes Script
     GetNotes

---
+++ Visual Novel/Assets/Scripts/Notes.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using Ink.Runtime;
+
+public class Notes : MonoBehaviour
+{
+    public void GetNotes()
+    {
+        Debug.Log("TENES LAS NOTAS");
+        GameEventsManager.Instance.dialogueEvents.UpdateInkDialogueVariable("QuestState", 
+            new StringValue(QuestState.CAN_FINISH.ToString()));
+        
+        Destroy(gameObject , 1f);
+    }
+}
