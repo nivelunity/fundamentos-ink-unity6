a86dc22e07238ab5f861b20059708cbd3e72807b
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-02-26 14:44:41 -0300

    Reposition Player

--- Visual Novel/Assets/Scripts/NavigationManager.cs
+++ Visual Novel/Assets/Scripts/NavigationManager.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 using Unity.Cinemachine;
+using UnityEngine.Serialization;
 
 public class NavigationManager : MonoBehaviour
 {
     [SerializeField] private Transform[] rooms;
-    [SerializeField] private CinemachineCamera camera;
+    [SerializeField] private CinemachineCamera cinemachineCamera;
     [SerializeField] private Transform player;
 
     [SerializeField]
     [Range(0,2)]
     private int roomNumber = 1;
-
+    
+    
+    [SerializeField]
+    [Range(10,50)]
+    private float positionOffset = 30f;
+    
     public void GoNextRoom()
     {
         int nextRoomNumber = roomNumber + 1;
@@ -18,7 +24,9 @@ public class NavigationManager : MonoBehaviour
         if(nextRoomNumber == rooms.Length) return;
 
         roomNumber = nextRoomNumber;
-        camera.Target.TrackingTarget = rooms[roomNumber];
+        cinemachineCamera.Target.TrackingTarget = rooms[roomNumber];
+        
+        RepositionPlayer(positionOffset);
     }
     
     public void GoPrevRoom()
@@ -29,6 +37,14 @@ public class NavigationManager : MonoBehaviour
         if(prevRoomNumber < 0) return;
 
         roomNumber = prevRoomNumber;
-        camera.Target.TrackingTarget = rooms[roomNumber];
+        cinemachineCamera.Target.TrackingTarget = rooms[roomNumber];
+        
+        RepositionPlayer(-positionOffset);
+    }
+
+    private void RepositionPlayer(float offset)
+    {
+        float newX = player.position.x + offset;
+        player.position = new Vector3(newX, player.position.y, player.position.z);
     }
 }
