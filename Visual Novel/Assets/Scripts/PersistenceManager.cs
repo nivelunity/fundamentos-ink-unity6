52b9ce2a53ee9f0e5d0fd757ee03b3781635e995
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-03-03 14:54:39 -0300

    PersistenceManager Singleton

--- Visual Novel/Assets/Scripts/PersistenceManager.cs
+++ Visual Novel/Assets/Scripts/PersistenceManager.cs
@@ -3,10 +3,25 @@ using Ink.Runtime;
 
 public class PersistenceManager : MonoBehaviour
 {
+   public static PersistenceManager Instance { get; private set; }
+   
    private const string saveVariablesKey = "INK_VARIABLES";
 
+   private void Awake()
+   {
+      if (Instance != null)
+      {
+         Debug.LogError(" Instancia duplicada de PersistenceManager "+ transform + " - " +Instance);
+         Destroy(gameObject);
+         return;
+      }
+
+      Instance = this;
+   }
+   
    public void SaveStoryState(Story story)
    {
+      Debug.Log("Save Story State");
       PlayerPrefs.SetString(saveVariablesKey, story.state.ToJson());
    }
    
@@ -14,6 +29,7 @@ public class PersistenceManager : MonoBehaviour
    {
       if (PlayerPrefs.HasKey(saveVariablesKey))
       {
+         Debug.Log("Load Story State");
          string jsonState = PlayerPrefs.GetString(saveVariablesKey);
          story.state.LoadJson(jsonState);
       }
