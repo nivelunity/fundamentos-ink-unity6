b12deb0d27e971d4cac6b6ae37fc89605903270a
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-03-03 14:52:03 -0300

    TryLoadStoryState Method
     string jsonState = PlayerPrefs.GetString(saveVariablesKey);
     story.state.LoadJson(jsonState);

--- Visual Novel/Assets/Scripts/PersistenceManager.cs
+++ Visual Novel/Assets/Scripts/PersistenceManager.cs
@@ -5,8 +5,17 @@ public class PersistenceManager : MonoBehaviour
 {
    private const string saveVariablesKey = "INK_VARIABLES";
 
-   public void SaveVariables(Story story)
+   public void SaveStoryState(Story story)
    {
       PlayerPrefs.SetString(saveVariablesKey, story.state.ToJson());
+   }
+   
+   public void TryLoadStoryState(Story story)
+   {
+      if (PlayerPrefs.HasKey(saveVariablesKey))
+      {
+         string jsonState = PlayerPrefs.GetString(saveVariablesKey);
+         story.state.LoadJson(jsonState);
+      }
    }
 }
